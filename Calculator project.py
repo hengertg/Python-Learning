5aa6c614a923e6feb4458e19395e87a53296a95c
hengertg <hengertg@gmail.com>
2026-06-20 08:26:21 -0400

    Fixing errors with the arithmetics, still more errors to fix that is on tils for exmaple in casse that the user want to divide by 0 a number

--- Calculator project.py
+++ Calculator project.py
@@ -1,6 +1,3 @@
-import math
-
-
 print(f"Henger Calculator")
 
 while True:
@@ -8,45 +5,53 @@ while True:
     print("+ is for plus - is for minus * is for time / si for divied by ** is for exponentiation % is for modulus // is for floor division ")
     UserNum =input("Type a Symbol ")
 
+    ValidsArithmetics = ["+", "-", "*", "%", "//", "**", "/"]
+
+    while UserNum not in ValidsArithmetics:
+        
+     print("Invalid symbol please introduce the right symbol.")
+
+     UserNum =input("Type a Symbol ")
+
     num1=int(input("Type a number: "))
     num2=int(input("Type your second number: "))
 
 
-    plus = "+"
-    minus = "-"
-    time = "*"
-    modulus = "%"
-    floor_division = "//"
-    exponentiation = "**"
+
+
 
 
     def result():
-     if UserNum == "+":
-        result = num1 + num2
-        return result
+        if UserNum == "+":
+            result = num1 + num2
+            return result
 
     
-     if UserNum == "-":
-        result = num1 - num2
-        return result
+        if UserNum == "-":
+            result = num1 - num2
+            return result
     
-     if UserNum == "*":
-        result = num1 * num2
-        return result
+        if UserNum == "*":
+            result = num1 * num2
+            return result
     
-     if UserNum == "%":
-        result = num1 % num2
-        return result
+        if UserNum == "%":
+            result = num1 % num2
+            return result
     
-     if UserNum == "//":
-        result = num1 // num2
-        return result
+        if UserNum == "//":
+            result = num1 // num2
+            return result
     
-     if UserNum == "**":
-        result = num1 ** num2
-        return result
-
+        if UserNum == "**":
+            result = num1 ** num2
+            return result
+            
+            
     print(f"The result is: {result()}")
+
+
+
 
 
 
