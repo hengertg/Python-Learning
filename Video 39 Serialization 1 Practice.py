40e5e250f5b18cb671f345e686d3c49a062d8cdc
hengertg <hengertg@gmail.com>
2026-07-10 17:20:36 -0400

    Serialization 1

---
+++ Video 39 Serialization 1 Practice.py
@@ -0,0 +1,38 @@
+import pickle
+
+lista_nombres = ["Pedro", "Sara", "Maria", "Isabel"]
+
+
+# Para dar a entender que un archivo es de escritura binaria usamos wb esto significa write binary o eso le da a entender a python que es un archivo de escritura binaria
+
+fichero_binario = open("lista_nombres","wb")
+
+
+# para usar el metodo dump debemos primero usar la libreria pickle + .dump() dentro de los parentesis van el archivo y el nombre del archivo como parametros
+
+# pickle.dump(lista_nombres, fichero_binario)
+
+# fichero_binario.close()
+
+# Para borrar el fichero de la memoria usamos del()
+
+del (fichero_binario)
+
+
+# =========================================================================================
+#
+# Para rescatar un archivo que ya esta dumped con .dump() y poder leerlo es sencillo
+
+# Debemos manter el import de pickle o importarlo
+#
+# luego una variable + = + nombre del archivo +("Nombre del arcivo", "rb") rb vendria significando read binary
+#
+# Por ejemplo
+
+fichero = open("lista_nombres", "rb")
+
+# Luego usamos el method de pickle que es pickle.load() dentro de los parentesis que vendria siendo el metodo donde tenemos guardado el fichero
+
+lista = pickle.load(fichero)
+
+print(lista)
---
+++ Video 39 Serialization 1.py
@@ -0,0 +1,19 @@
+# ====================================================================================
+#
+# QUE ES LA SERIALIZATION?
+# ====================================================================================
+#
+# La serialización es el proceso de convertir un objeto o estructura de datos en un formato plano
+#
+# Consiste en guardar en un fichero externo una coleccion, un diccionario o un objeto codificado en codigo binarios osea en una succesion de bytes
+#
+# Esto permite que la distribucion por internet sea mas facil ya que esta en codigo binario
+# ====================================================================================
+#
+# Para poder llevar a cabo este proceso en Python debemos usar una bibloteca en Python que es Pickle
+#
+# Los metodos mas importantes que usaremos en esta ocasion es
+#
+# dump() es volcado de datos al fichero binario externo
+#
+# load() es carga de los datos del fichero binario externo
